c2fb85d36a14f52567b384594d7699abbd5aa4c3
Ritika <ritikarathi2005@gmail.com>
2026-07-01 20:20:12 +0530

    Auth controller

---
+++ Assignment/src/main/java/com/example/Assignment/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.example.Assignment.controller;
+
+import com.example.Assignment.dto.Request.LoginRequest;
+import com.example.Assignment.dto.Request.RegisterRequest;
+import com.example.Assignment.dto.Response.AuthResponse;
+import com.example.Assignment.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public AuthResponse register(
+            @Valid @RequestBody RegisterRequest request) {
+
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(
+            @Valid @RequestBody LoginRequest request) {
+
+        return authService.login(request);
+    }
+}
