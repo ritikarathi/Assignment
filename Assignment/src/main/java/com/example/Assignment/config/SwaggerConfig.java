07e3c20b44a1b7e4f744095c7b698ba28dd34aea
Ritika <ritikarathi2005@gmail.com>
2026-07-01 20:35:24 +0530

    exception+swagger config

--- Assignment/src/main/java/com/example/Assignment/config/SwaggerConfig.java
+++ Assignment/src/main/java/com/example/Assignment/config/SwaggerConfig.java
@@ -1,4 +1,26 @@
 package com.example.Assignment.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("REST API with JWT Authentication")
+                        .version("1.0")
+                        .description("Scalable REST API with Authentication & Role-Based Access")
+                        .contact(new Contact()
+                                .name("Ritika Rathi")
+                                .email("your-email@example.com")
+                        )
+                );
+    }
 }
---
+++ Assignment/src/main/java/com/example/Assignment/exeption/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.example.Assignment.exeption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntimeException(RuntimeException ex) {
+
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("message", ex.getMessage());
+
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationException(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
