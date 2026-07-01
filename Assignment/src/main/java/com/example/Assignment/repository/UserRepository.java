300db5195ace87814c02d560fdf839d576b8d971
Ritika <ritikarathi2005@gmail.com>
2026-07-01 11:30:08 +0530

    Repos initialised

---
+++ Assignment/src/main/java/com/example/Assignment/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.Assignment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
